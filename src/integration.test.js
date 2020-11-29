3a4547eb61e5ab0d85cea9f9ccf6e713db1fd378
Jan Procházka <honza.spacir@gmail.com>
2020-11-29 02:30:27 +0100

    Strict mode (#2)
    * Added strict flag + catch errors
    * Duplicate tests for non-strict mode

--- src/integration.test.js
+++ src/integration.test.js
@@ -6,6 +6,7 @@ const fakeRNG = (min, max) => Math.floor((max + min) / 2);
 
 describe.each([
     // VALID CASES
+    ["1d5", fakeRNG(1, 5)],
     ["d5", fakeRNG(1, 5)],
     ["-d5", -fakeRNG(1, 5)],
     ["10d6 + 20", 10 * fakeRNG(1, 6) + 20],
@@ -27,22 +28,61 @@ describe.each([
     ["5dd6", new Error(`Unexpected token "d" after "d".`)],
     ["(5d6", new Error(`Expected a matching ")" after "6".`)],
     ["(1/0)d6", new Error(`Too many rolls.`)],
-])("evaluate(%p)", (input, expected) => {
+])("evaluate(%p) strict", (input, expected) => {
+    const options = { limit: Infinity, rng: fakeRNG };
     if (expected instanceof Error) {
         it(`throws "${expected.message}"`, () => {
-            expect(() => evaluate(input, Infinity, fakeRNG)).toThrow(expected);
+            expect(() => evaluate(input, options)).toThrow(expected);
         });
     } else {
         it(`returns ${expected}`, () => {
-            expect(evaluate(input, Infinity, fakeRNG)).toEqual(expected);
+            expect(evaluate(input, options)).toEqual(expected);
         });
     }
 });
 
 describe("default RNG evaluate(%p)", () => {
+    // this is to satisfy coverage
     globalThis.Math.random = () => 1;
 
     it(`returns 5`, () => {
         expect(evaluate("1d5")).toEqual(5);
     });
+});
+
+describe.each([
+    // VALID CASES
+    ["1d5", fakeRNG(1, 5)],
+    ["d5", fakeRNG(1, 5)],
+    ["-d5", -fakeRNG(1, 5)],
+    ["10d6 + 20", 10 * fakeRNG(1, 6) + 20],
+    ["(50* 50)d( 10 +10)", 50 * 50 * fakeRNG(1, 10 + 10)],
+    [
+        "((14+14d37)+(((45-45d62)-(32-32d56))*(((19-19d22)+((37*37d66)-((29*11)/(48-48d54))))-((((14/8)*(91+3))+((17/17d17)*(69-69d100)))*(((44*10)-(85-5))*((76+1)-(55-8)))))))",
+        -1130235126,
+    ],
+
+    // INVALID CASES
+    [
+        "((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((",
+        null,
+    ],
+    ["5d-", null],
+    ["6e5", null],
+    ["", null],
+    ["3d10)", null],
+    ["5dd6", null],
+    ["(5d6", null],
+    ["(1/0)d6", null],
+])("evaluate (%p) non-strict", (input, expected) => {
+    const options = { limit: Infinity, rng: fakeRNG, strict: false };
+    if (expected === null) {
+        it(`returns null`, () => {
+            expect(evaluate(input, options)).toEqual(expected);
+        });
+    } else {
+        it(`returns ${expected}`, () => {
+            expect(evaluate(input, options)).toEqual(expected);
+        });
+    }
 });
